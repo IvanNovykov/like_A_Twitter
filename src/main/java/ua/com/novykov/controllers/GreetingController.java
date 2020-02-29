c51ece1b8d15ae2239dfab07d29aa1c0a35ee65b
IvanNovykov <ivan.novykov@gmail.com>
2020-02-29 06:50:47 +0200

    thymeleaf changed to mustache

--- src/main/java/ua/com/novykov/controllers/GreetingController.java
+++ src/main/java/ua/com/novykov/controllers/GreetingController.java
@@ -5,11 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class GreetingController {
 @GetMapping("/greeting")
-    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "Ivan")String name, Model model){
-    model.addAttribute("name", name);
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "Ivan")String name, Map<String, Object> model){
+    model.put("name", name);
     return "greeting";
 }
+
+@GetMapping
+    public String main(Map<String, Object> model){
+    model.put("some", "hello, my dear friend Ivan");
+    return "main";
+}
+
 }
